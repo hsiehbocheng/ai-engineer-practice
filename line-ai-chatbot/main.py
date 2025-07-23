af26492c971a685e591020ae853d898e47d2c89e
Benson <bochenghsieh@gmail.com>
2025-07-24 01:04:28 +0800

    add packages

--- line-ai-chatbot/main.py
+++ line-ai-chatbot/main.py
@@ -3,3 +3,12 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import TextSendMessage
 import os, dotenv
+
+dotenv.load_dotenv()  # 讀取 .env
+
+app = FastAPI()
+bot_api = LineBotApi(os.getenv("LINE_ACCESS_TOKEN"))
+handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+
+print(bot_api)
+print(handler)
